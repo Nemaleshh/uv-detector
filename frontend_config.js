be92ad0c2dfc0b44a61534bb49fdb5f6f320c542
Nemaleshh <nemaleshwargithub@gmail.com>
2026-03-16 12:00:33 +0530

    success

--- frontend_config.js
+++
@@ -1,7 +0,0 @@
-// API Configuration
-const API_BASE_URL = 'http://localhost:5000';
-
-// Change this to your machine's IP for testing from phone
-// Example: const API_BASE_URL = 'http://192.168.1.100:5000';
-
-console.log('API Base URL:', API_BASE_URL);
